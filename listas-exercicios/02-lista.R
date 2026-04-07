959a7cb58e7160882ea60540e6eee867ac39c73b
leticia.rodrigues0712 <rodriguesleticia0712@gmail.com>
2026-04-07 20:17:47 -0300

    atualizações após a aula de 07/04

--- listas-exercicios/02-lista.R
+++ listas-exercicios/02-lista.R
@@ -11,7 +11,9 @@
 options(digits = 5, scipen = 999)
 
 # carrega os pacotes necessários
-
+library(here)
+library(tidyverse)
+library(janitor)
 
 # Exercicio 1 -------------------------------------------------------------
 
@@ -19,36 +21,51 @@ options(digits = 5, scipen = 999)
 ## a)
 
 
-## b)
 
+## b)
+caminho_csv <- here("dados/brutos/dados-marketing.csv")
+
+dados_marketing <- read_csv(caminho_csv)
 
 ## c)
-
+glimpse(dados_marketing)
 
 
 # Exercicio 2 -------------------------------------------------------------
 
-## a)
+## a) Representa alguns gastos e receitas do mês.
 
 
-## b)
+## b) 9 variáveis e 156 observações
 
 
 # Exercicio 3 -------------------------------------------------------------
 
 ## a)
-
+dados_marketing_limpos <- dados_marketing |> 
+  clean_names()
+  
 
 ## b)
+  names(dados_marketing_limpos)
 
 
 
 # Exercicio 4 -------------------------------------------------------------
-
+dados_marketing_limpos |> 
+  select("Data", "Mês", "Gasto TV", "Gasto Rádio", "Promoção", "Receita Vendas")
+  
 
 
 # Exercicio 5 -------------------------------------------------------------
-
+  dados_marketing_limpos <- dados)_marketing_limpos|> 
+    mutate(
+      gasto_total = gasto_tv + gasto_radio + gasto_redes_sociais + gasto_email
+      )
+           
+  select(data, mes, gasto_total, receita_vendas)
+  View(dados_marketing_limpos)
+  
 
 
 # Exercicio 6 -------------------------------------------------------------
